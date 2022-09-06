bc53880bfda0a89e7551ef348bb0981d3937f25c
GregoryDushin <86513789+GregoryDushin@users.noreply.github.com>
2022-09-06 14:44:03 +0300

    Image Picker Controller

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,92 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Григорий Душин on 06.09.2022.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    //MARK: TableView Delegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let cameraIcon = UIImage(named: "camera")
+            let photoIcon = UIImage(named: "photo")
+            
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+
+            }
+            camera.setValue(cameraIcon, forKey: "image")
+           
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            photo.setValue(photoIcon, forKey: "image")
+          
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+  
+
+}
+
+
+//MARK: TextField Delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    //скрываем клавиатуру по нажатию на Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //скрываем клавиатуру по нажатию на ячейку
+    
+    
+    
+}
+
+//MARK: Work with image
+
+extension NewPlaceViewController : UIImagePickerControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
+    }
+}
